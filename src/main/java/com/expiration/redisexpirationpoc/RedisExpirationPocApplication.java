e2022c9729027e654ce62683da57350dedbe92eb
Franklin Bourguignon <franklin.bourguignon@zoop.com.br>
2021-05-24 11:11:26 -0300

    commit inicial do projeto gerado pelo gitignore

---
+++ src/main/java/com/expiration/redisexpirationpoc/RedisExpirationPocApplication.java
@@ -0,0 +1,13 @@
+package com.expiration.redisexpirationpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisExpirationPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisExpirationPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/expiration/redisexpirationpoc/RedisExpirationPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expiration.redisexpirationpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisExpirationPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
